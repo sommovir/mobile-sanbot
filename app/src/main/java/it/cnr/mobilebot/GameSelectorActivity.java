1ee87a4ab8f8a881dcd931c2d795f17f27451c08
AlessioPecilli <alessiopecilli@gmail.com>
2022-05-20 12:12:21 +0200

    Fixed Recycler view

--- app/src/main/java/it/cnr/mobilebot/GameSelectorActivity.java
+++ app/src/main/java/it/cnr/mobilebot/GameSelectorActivity.java
@@ -1,13 +1,17 @@
 package it.cnr.mobilebot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import it.cnr.mobilebot.game.GameAdapter;
 import it.cnr.mobilebot.logic.CognitiveGame;
 import it.cnr.mobilebot.logic.CognitiveGameListModel;
 
@@ -18,16 +22,14 @@ public class GameSelectorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_selector);
 
-        List<CognitiveGame> cognitiveGames = new ArrayList<>();
-        cognitiveGames.add(new CognitiveGame("Lista della Spesa", "questo gioco consiste nell'annoiarsi a morte"));
-        cognitiveGames.add(new CognitiveGame("Duck Nukem 3D", "gioco fighissimo anni 90"));
-        cognitiveGames.add(new CognitiveGame("Freecell", "gioco per dipendenti pubblici"));
+        RecyclerView recyclerView;
+        GameAdapter gameAdapter = new GameAdapter(this);
 
-        ListView listview = (ListView) findViewById(R.id.gameSelector);
-        if(listview == null){
-            System.out.println("NULLONE DEMMERDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        }
-        listview.setAdapter(new CognitiveGameListModel(this, cognitiveGames));
+        recyclerView = findViewById(R.id.RecycleGame);
+
+        recyclerView.setAdapter(gameAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
 
     }
 }
